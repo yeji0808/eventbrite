cd75162d87bf9b27bc493ff1f8072099c715d3ef
Yeji <yeji080808@gmail.com>
2017-12-09 16:40:30 +0900

    test

--- routes/events.js
+++ routes/events.js
@@ -25,7 +25,16 @@ router.get('/', catchErrors(async (req, res, next) => {
   if (term) {
     query = {$or: [
       {title: {'$regex': term, '$options': 'i'}},
-      {content: {'$regex': term, '$options': 'i'}}
+      {place: {'$regex': term, '$options': 'i'}},
+      {start: {'$regex': term, '$options': 'i'}},
+      {end: {'$regex': term, '$options': 'i'}},
+      {content: {'$regex': term, '$options': 'i'}},
+      {group_name: {'$regex': term, '$options': 'i'}},
+      {group_explain: {'$regex': term, '$options': 'i'}},
+      {koe: {'$regex': term, '$options': 'i'}},
+      {inlineRadioOptions2: {'$regex': term, '$options': 'i'}},
+      {inlineRadioOptions3: {'$regex': term, '$options': 'i'}},
+      {price: {'$regex': term, '$options': 'i'}}
     ]};
   }
   const events = await Events.paginate(query, {
@@ -46,10 +55,12 @@ router.get('/:id/edit', needAuth, catchErrors(async (req, res, next) => {
 }));
 
 router.get('/:id', catchErrors(async (req, res, next) => {
-  const event = await Events.findById(req.params.id).populate('author');
+  console.log(req.params);
+
+    const event = await Events.findById(req.params.id).populate('author');
   const answers = await Answer.find({event: event.id}).populate('author');
   event.numReads++;    // TODO: 동일한 사람이 본 경우에 Read가 증가하지 않도록???
-
+console.log(event);
   await event.save();
   res.render('events/show', {event: event, answers: answers});
 }));
@@ -62,7 +73,16 @@ router.put('/:id', catchErrors(async (req, res, next) => {
     return res.redirect('back');
   }
   event.title = req.body.title;
+  event.place = req.body.place;
+  event.start = req.body.start;
+  event.end = req.body.end;
   event.content = req.body.content;
+  event.group_name = req.body.group_name;
+  event.group_explain = req.body.group_explain;
+  event.koe =  req.body.koe;
+  event.inlineRadioOptions2 =  req.body.inlineRadioOptions2;
+  event.inlineRadioOptions3 =  req.body.inlineRadioOptions3;
+  event.price = req.body.price;
   event.tags = req.body.tags.split(" ").map(e => e.trim());
 
   await event.save();
@@ -80,9 +100,18 @@ router.post('/', needAuth, catchErrors(async (req, res, next) => {
   const user = req.user;
   var event = new Events({
     title: req.body.title,
-    author: user._id,
+    place: req.body.place,
+    start: req.body.start,
+    end: req.body.end,
     content: req.body.content,
-    tags: req.body.tags.split(" ").map(e => e.trim()),
+    group_name: req.body.group_name,
+    group_explain: req.body.group_explain,
+    koe: req.body.koe,
+    inlineRadioOptions2: req.body.inlineRadioOptions2,
+    inlineRadioOptions3: req.body.inlineRadioOptions3,
+    price: req.body.price,
+    author: user._id,
+    tags: req.body.tags.split(" ").map(e => e.trim())
   });
   await event.save();
   req.flash('success', 'Successfully posted');
@@ -90,6 +119,7 @@ router.post('/', needAuth, catchErrors(async (req, res, next) => {
 }));
 
 router.post('/:id/answers', needAuth, catchErrors(async (req, res, next) => {
+  console.log('body 내용: ',req.body);
   const user = req.user;
   const event = await Events.findById(req.params.id);
 
@@ -107,7 +137,7 @@ router.post('/:id/answers', needAuth, catchErrors(async (req, res, next) => {
   event.numAnswers++;
   await event.save();
 
-  req.flash('success', 'Successfully answered');
+  req.flash('success', 'Successfully registered');
   res.redirect(`/events/${req.params.id}`);
 }));
 
